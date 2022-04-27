d27b2b78f432805741c782d7323c0677de2eb491
coaxial <c@64b.it>
2022-04-27 09:35:29 +0200

    Restore Transmission

--- servarr/molecule/default/tests/test_default.py
+++ servarr/molecule/default/tests/test_default.py
@@ -31,3 +31,31 @@ def test_nzbget_config(host):
     assert f.user == 'nzbget'
     assert f.group == 'nzbget'
     assert f.contains('# Test conf file')
+
+
+def test_nzbget_service(host):
+    assert host.service('nzbget').is_running
+
+
+def test_transmission_files(host):
+    files = ['stats.json', 'settings.json']
+    missing_files = ['dht.dat']
+
+    for file in files:
+        f = host.file('/home/debian-transmission/.config/transmission-daemon/'
+                      + file)
+        assert f.mode == 0o600
+        assert f.user == 'debian-transmission'
+        assert f.group == 'debian-transmission'
+
+        if file == 'stats.json':
+            assert f.content_string == "{}"
+
+    for mfile in missing_files:
+        mf = host.file('/home/debian-transmission/.config/transmission-daemon/'
+                       + mfile)
+        assert not mf.exists
+
+
+def test_transmission_service(host):
+    assert host.service('transmission-daemon').is_running
