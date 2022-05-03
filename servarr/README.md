385488fdfeb9520eeb27020523ed00406cb096a0
coaxial <c@64b.it>
2022-05-03 15:28:43 +0200

    wip

--- servarr/README.md
+++ servarr/README.md
@@ -11,18 +11,7 @@ Sets up a Servarr instance with:
 
 ### NZBGet
 
-If any of these files exist at `files/nzbget`:
-
-- nzbget.conf
-- stats
-- history
-
-They will be copied over existing files on the instance. A backup of the
-original will be generated. Be careful not to overwrite existing data.
-
-`nzbget.conf` should be manually edited beforehand to make sure paths etc. match.
-Another option is to not restore the config file through this playbook, and use
-the GUI instead to selectively restore parts of the config as per these
+Use the GUI to selectively restore parts of the config as per these
 instructions: https://nzbget.net/backup-and-restore-settings
 
 Default password is `nzbget`:`tegbnz6789` and the interface is accessible at
@@ -48,37 +37,14 @@ trailing `/`).
 
 ### Sonarr
 
-If any of these files exist at `files/sonarr`:
-
-- sonarr.db
-- sonarr.db-journal
-- nzbdrone.db
-- nzbdrone.db-journal
-- config.xml
-
-They will be copied over existing files on the instance. A backup of the
-original will be generated. Be careful not to overwrite existing data.
-
 `config.xml` should be manually edited beforehand to make sure the base URL is
-set to `/sonarr`. Another option is to not restore the config this playbook by
-leaving `files/sonarr` empty, and use the GUI instead to restore the config.
-Make sure the base URL is set to `/sonarr` in `config.xml` beforehand.
+set to `/sonarr`. Then restore the config using the GUI.
 
 The interface is accessible at `http://<ip>/sonarr`.
 
 ### Radarr
 
-If any of these files exist at `files/radarr`:
-
-- radarr.db
-- config.xml
-
-They will be copied over existing files on the instance. A backup of the
-original will be generated. Be careful not to overwrite existing data.
-
 `config.xml` should be manually edited beforehand to make sure the base URL is
-set to `/radarr`. Another option is to not restore the config this playbook by
-leaving `files/radarr` empty, and use the GUI instead to restore the config.
-Make sure the base URL is set to `/radarr` in `config.xml` beforehand.
+set to `/sonarr`. Then restore the config using the GUI.
 
 The interface is accessible at `http://<ip>/radarr`.
--- servarr/molecule/default/tests/test_default.py
+++ servarr/molecule/default/tests/test_default.py
@@ -6,35 +6,22 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_nzbget_history(host):
-    f = host.file('/mnt/downloads/nzb/data/queue/history')
+# def test_nzbget_history(host):
+#     f = host.file('/mnt/downloads/nzb/data/queue/history')
 
-    assert f.mode == 0o644
-    assert f.user == 'nzbget'
-    assert f.group == 'nzbget'
-    assert f.contains('nzbget diskstate file version 62\n3644')
-
-
-def test_nzbget_stats(host):
-    f = host.file('/mnt/downloads/nzb/data/queue/stats')
-
-    assert f.mode == 0o644
-    assert f.user == 'nzbget'
-    assert f.group == 'nzbget'
-    assert f.contains('news.example.org')
+#     assert f.mode == 0o644
+#     assert f.user == 'nzbget'
+#     assert f.group == 'nzbget'
+#     assert f.contains('nzbget diskstate file version 62\n3644')
 
 
-def test_nzbget_config(host):
-    f = host.file('/etc/nzbget.conf')
+# def test_nzbget_stats(host):
+#     f = host.file('/mnt/downloads/nzb/data/queue/stats')
 
-    assert f.mode == 0o644
-    assert f.user == 'nzbget'
-    assert f.group == 'nzbget'
-    assert f.contains('# Test conf file')
-
-
-def test_nzbget_service(host):
-    assert host.service('nzbget').is_running
+#     assert f.mode == 0o644
+#     assert f.user == 'nzbget'
+#     assert f.group == 'nzbget'
+#     assert f.contains('news.example.org')
 
 
 def test_transmission_files(host):
@@ -58,33 +45,20 @@ def test_transmission_files(host):
 
 
 def test_transmission_service(host):
-    assert host.service('transmission-daemon').is_running
+    s = host.service('transmission-daemon')
+    assert s.is_running
 
 
-def test_sonarr_config(host):
-    files = ['sonarr.db', 'config.xml']
+def test_firewall(host):
+    i = host.iptables
 
-    for file in files:
-        f = host.file('/var/lib/sonarr/' + file)
-
-        assert f.mode == 0o644
-        assert f.user == 'sonarr'
-        assert f.group == 'sonarr'
-
-
-def test_sonarr_service(host):
-    assert host.service('sonarr').is_running
-
-
-def test_radarr_config(host):
-    files = ['radarr.db', 'config.xml']
-
-    for file in files:
-        f = host.file('/var/lib/radarr/' + file)
-
-        assert f.mode == 0o644
-        assert f.user == 'radarr'
-
-
-def test_radarr_service(host):
-    assert host.service('radarr').is_running
+    assert (
+        '-A INPUT -p tcp -m tcp --dport 51413 '
+        '-m conntrack --ctstate NEW,ESTABLISHED '
+        '-m comment --comment "Allow Torrent traffic" -j ACCEPT'
+    ) in i.rules('filter', 'INPUT')
+    assert (
+        '-A OUTPUT -p tcp -m tcp --sport 51413 '
+        '-m conntrack --ctstate ESTABLISHED '
+        '-m comment --comment "Allow Torrent traffic" -j ACCEPT'
+    ) in i.rules('filter', 'OUTPUT')
